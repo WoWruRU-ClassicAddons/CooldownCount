3ecda94fe4538bc643c5350a766deba11454e006
M2us <Dead_Maus@mail.ru>
2017-06-20 19:03:46 +0300

    Added support ruRU locale ACE library deleted Added dependency from !Libs Added GUI interface

--- CooldownCount/CooldownCount.lua
+++ CooldownCount/CooldownCount.lua
@@ -1,6 +1,8 @@
 ﻿--[[ $Id: CooldownCount.lua 14691 2006-10-21 20:09:52Z damjau $ ]]--
 
 local L = AceLibrary("AceLocale-2.2"):new("CooldownCount")
+local waterfall = AceLibrary("Waterfall-1.0")
+
 local CooldownFont = {
 	["2002"] = "Fonts\\2002.TTF", 
 	["FRIZQT__"] = "Fonts\\FRIZQT__.TTF", 
@@ -22,150 +24,151 @@ function CooldownCount:OnInitialize()
 		color2 = {r=1, g=0, b=0},
 		size = { [1] = 18, [2] = 24, [3] = 28, [4] = 34 }
 	})
-	self:RegisterChatCommand(
-		{ "/cooldowncount", "/cc" },
-		{
-			type = "group",
-			args = {
-				reset = {
-					type = "execute",
-					name = L["reset"],
-					desc = L["Reset all settings."],
-					func = function()
-						self:ResetDB("profile")
-						self:Print(L["Reset to default settings."])
+	options = {
+		type = "group",
+		args = {
+			reset = {
+				type = "execute",
+				name = L["reset"],
+				desc = L["Reset all settings."],
+				func = function()
+					self:ResetDB("profile")
+					self:Print(L["Reset to default settings."])
+				end
+			},
+			shine = {
+				type = "toggle",
+				name = L["shine"],
+				desc = L["Toggle icon shine display after finish cooldown."],
+				get = function() return self.db.profile.shine end,
+				set = function(t) self.db.profile.shine = t end
+			},
+			shinescale = {
+				type = "range",
+				name = L["shinescale"],
+				desc = L["Adjust icon shine scale."],
+				min = 0,
+				max = 100,
+				step = 0.1,
+				get = function() return self.db.profile.shineScale end,
+				set = function(r) self.db.profile.shineScale = r end
+			},
+			minimum_duration = {
+				type = "range",
+				name = L["minimum duration"],
+				desc = L["Minimum duration for display cooldown count."],
+				min = 0.5,
+				max = 100,
+				step = 0.5,
+				get = function() return self.db.profile.minimumDuration end,
+				set = function(r) self.db.profile.minimumDuration = r end
+			},
+			hide_animation = {
+				type = "toggle",
+				name = L["hide animation"],
+				desc = L["Hide Bliz origin cooldown animation."],
+				get = function() return self.db.profile.hideAnimation end,
+				set = function(t) self.db.profile.hideAnimation = t end
+			},
+			font = {
+				type = "text",
+				name = L["font"],
+				desc = L["Set cooldown value display font."],
+				get = function() return self.db.profile.font end,
+				set = function(f) self.db.profile.font = f end,
+				validate =  {L["2002"], L["FRIZQT__"], L["ARIALN"], L["K_Damage"], L["K_Pagetext"], },
+			},
+			color1 = {
+				type = "text",
+				name = L["common color"],
+				desc = L["Setup the common color for value display."],
+				usage = "<r=n, g=n, b=n>",
+				get = function()
+					return "r="..self.db.profile.color1.r.." g="..self.db.profile.color1.g.." b="..self.db.profile.color1.b
+				end,
+				set = function(c)
+					c = string.lower(c)
+					local color = {}
+					for k,v in string.gfind(c, "([r|g|b])=([%d\.]+)") do
+						color[k]=v
 					end
-				},
-				shine = {
-					type = "toggle",
-					name = L["shine"],
-					desc = L["Toggle icon shine display after finish cooldown."],
-					get = function() return self.db.profile.shine end,
-					set = function(t) self.db.profile.shine = t end
-				},
-				shinescale = {
-					type = "range",
-					name = L["shinescale"],
-					desc = L["Adjust icon shine scale."],
-					min = 0,
-					max = 100,
-					step = 0.1,
-					get = function() return self.db.profile.shineScale end,
-					set = function(r) self.db.profile.shineScale = r end
-				},
-				minimum_duration = {
-					type = "range",
-					name = L["minimum duration"],
-					desc = L["Minimum duration for display cooldown count."],
-					min = 0.5,
-					max = 100,
-					step = 0.5,
-					get = function() return self.db.profile.minimumDuration end,
-					set = function(r) self.db.profile.minimumDuration = r end
-				},
-				hide_animation = {
-					type = "toggle",
-					name = L["hide animation"],
-					desc = L["Hide Bliz origin cooldown animation."],
-					get = function() return self.db.profile.hideAnimation end,
-					set = function(t) self.db.profile.hideAnimation = t end
-				},
-				font = {
-					type = "text",
-					name = L["font"],
-					desc = L["Set cooldown value display font."],
-					get = function() return self.db.profile.font end,
-					set = function(f) self.db.profile.font = f end,
-					validate =  {L["2002"], L["FRIZQT__"], L["ARIALN"], L["K_Damage"], L["K_Pagetext"], },
-				},
-				color1 = {
-					type = "text",
-					name = L["common color"],
-					desc = L["Setup the common color for value display."],
-					usage = "<r=n, g=n, b=n>",
-					get = function()
-						return "r="..self.db.profile.color1.r.." g="..self.db.profile.color1.g.." b="..self.db.profile.color1.b
-					end,
-					set = function(c)
-						c = string.lower(c)
-						local color = {}
-						for k,v in string.gfind(c, "([r|g|b])=([%d\.]+)") do
-							color[k]=v
-						end
-						if (color.r and color.g and color.b) then
-							self.db.profile.color1={r=color.r, g=color.g, b=color.b}
-						end
+					if (color.r and color.g and color.b) then
+						self.db.profile.color1={r=color.r, g=color.g, b=color.b}
 					end
-				},
-				color2 = {
-					type = "text",
-					name = L["warning color"],
-					desc = L["Setup the warning color for value display."],
-					usage = "<r=n, g=n, b=n>",
-					get = function()
-						return "r="..self.db.profile.color2.r.." g="..self.db.profile.color2.g.." b="..self.db.profile.color2.b
-					end,
-					set = function(c)
-						c = string.lower(c)
-						local color = {}
-						for k,v in string.gfind(c, "([r|g|b])=([%d\.]+)") do
-							color[k]=v
-						end
-						if (color.r and color.g and color.b) then
-							self.db.profile.color2={r=color.r, g=color.g, b=color.b}
-						end
+				end
+			},
+			color2 = {
+				type = "text",
+				name = L["warning color"],
+				desc = L["Setup the warning color for value display."],
+				usage = "<r=n, g=n, b=n>",
+				get = function()
+					return "r="..self.db.profile.color2.r.." g="..self.db.profile.color2.g.." b="..self.db.profile.color2.b
+				end,
+				set = function(c)
+					c = string.lower(c)
+					local color = {}
+					for k,v in string.gfind(c, "([r|g|b])=([%d\.]+)") do
+						color[k]=v
 					end
-				},
-				size = {
-					type = "group",
-					name = L["font size"],
-					desc = L["Setup cooldown value display font size."],
-					args = {
-						small = {
-							type = "range",
-							name = L["small size"],
-							desc = L["Small font size for cooldown is longer than 10 minutes."],
-							min = 10,
-							max = 45,
-							step = 1,
-							get = function() return self.db.profile.size[1] end,
-							set = function(r) self.db.profile.size[1] = r end
-						},
-						medium = {
-							type = "range",
-							name = L["medium size"],
-							desc = L["Medium font size for cooldown is longer than 1 minute and less than 10 minutes."],
-							min = 10,
-							max = 45,
-							step = 1,
-							get = function() return self.db.profile.size[2] end,
-							set = function(r) self.db.profile.size[2] = r end
-						},
-						large = {
-							type = "range",
-							name = L["large size"],
-							desc = L["Large font size for cooldown is longer than 10 seconds and less than 1 minutes."],
-							min = 10,
-							max = 45,
-							step = 1,
-							get = function() return self.db.profile.size[3] end,
-							set = function(r) self.db.profile.size[3] = r end
-						},
-						warn = {
-							type = "range",
-							name = L["warning size"],
-							desc = L["Warning font size for cooldown is less than 10 seconds."],
-							min = 10,
-							max = 45,
-							step = 1,
-							get = function() return self.db.profile.size[4] end,
-							set = function(r) self.db.profile.size[4] = r end
-						}
+					if (color.r and color.g and color.b) then
+						self.db.profile.color2={r=color.r, g=color.g, b=color.b}
+					end
+				end
+			},
+			size = {
+				type = "group",
+				name = L["font size"],
+				desc = L["Setup cooldown value display font size."],
+				args = {
+					small = {
+						type = "range",
+						name = L["small size"],
+						desc = L["Small font size for cooldown is longer than 10 minutes."],
+						min = 10,
+						max = 45,
+						step = 1,
+						get = function() return self.db.profile.size[1] end,
+						set = function(r) self.db.profile.size[1] = r end
+					},
+					medium = {
+						type = "range",
+						name = L["medium size"],
+						desc = L["Medium font size for cooldown is longer than 1 minute and less than 10 minutes."],
+						min = 10,
+						max = 45,
+						step = 1,
+						get = function() return self.db.profile.size[2] end,
+						set = function(r) self.db.profile.size[2] = r end
+					},
+					large = {
+						type = "range",
+						name = L["large size"],
+						desc = L["Large font size for cooldown is longer than 10 seconds and less than 1 minutes."],
+						min = 10,
+						max = 45,
+						step = 1,
+						get = function() return self.db.profile.size[3] end,
+						set = function(r) self.db.profile.size[3] = r end
+					},
+					warn = {
+						type = "range",
+						name = L["warning size"],
+						desc = L["Warning font size for cooldown is less than 10 seconds."],
+						min = 10,
+						max = 45,
+						step = 1,
+						get = function() return self.db.profile.size[4] end,
+						set = function(r) self.db.profile.size[4] = r end
 					}
 				}
 			}
 		}
-	)
+	}
+	self:RegisterChatCommand({"/cooldowncountcl", "/cccl"}, options) 
+	self:RegisterChatCommand({"/cooldowncount", "/cc"}, function() waterfall:Open('CooldownCount') end)
+	waterfall:Register('CooldownCount', 'aceOptions', options, 'title','CooldownCount 1.1. Revision: 16491','colorR', 0.2, 'colorG', 0.6, 'colorB', 0.2) 
+
 end
 
 function CooldownCount:OnEnable()
@@ -178,7 +181,7 @@ function CooldownCount:CooldownFrame_SetTimer(cooldownFrame, start, duration, en
 	else
 		cooldownFrame:Hide()
 	end
-
+	
 	if start > 0 and duration > self.db.profile.minimumDuration and enable > 0 then
 		local cooldownCount = cooldownFrame.textFrame or CooldownCount:CreateCooldownCount(cooldownFrame, start, duration)
 		cooldownCount.start = start
@@ -193,38 +196,38 @@ end
 function CooldownCount:CreateCooldownCount(cooldown, start, duration)
 	local textFrame = CreateFrame("Frame", nil, cooldown:GetParent())
 	cooldown.textFrame = textFrame
-
+	
 	textFrame:SetAllPoints(cooldown:GetParent())
 	textFrame:SetFrameLevel(cooldown.textFrame:GetFrameLevel() + 1)
-
+	
 	textFrame.text = cooldown.textFrame:CreateFontString(nil, "OVERLAY")
 	textFrame.text:SetPoint("CENTER", cooldown.textFrame, "CENTER", 0, -1)
-
+	
 	textFrame.icon =
-		--standard action button icon, $parentIcon
-		getglobal(cooldown:GetParent():GetName() .. "Icon") or
-		--standard item button icon,  $parentIconTexture
-		getglobal(cooldown:GetParent():GetName() .. "IconTexture") or
-		--discord action button, $parent_Icon
-		getglobal(cooldown:GetParent():GetName() .. "_Icon")
-
+	--standard action button icon, $parentIcon
+	getglobal(cooldown:GetParent():GetName() .. "Icon") or
+	--standard item button icon,  $parentIconTexture
+	getglobal(cooldown:GetParent():GetName() .. "IconTexture") or
+	--discord action button, $parent_Icon
+	getglobal(cooldown:GetParent():GetName() .. "_Icon")
+	
 	if textFrame.icon then
 		textFrame:SetScript("OnUpdate", CooldownCount.Text_OnUpdate)
 	end
-
+	
 	textFrame:Hide()
-
+	
 	return textFrame
 end
 
 function CooldownCount:Text_OnUpdate()
 	if this.timeToNextUpdate <= 0 or not this.icon:IsVisible() then
 		local remain = this.duration - (GetTime() - this.start)
-
+		
 		if floor(remain + 0.5) > 0 and this.icon:IsVisible() then
 			local text, toNextUpdate, size, twoColor = CooldownCount:GetFormattedTime(remain)
 			this.text:SetFont(CooldownFont[CooldownCount.db.profile.font], size, "OUTLINE")
-
+			
 			color = CooldownCount.db.profile.color1
 			if twoColor then
 				if this.twoColor then
@@ -235,7 +238,7 @@ function CooldownCount:Text_OnUpdate()
 				end
 			end
 			this.text:SetTextColor(color.r, color.g, color.b)
-
+			
 			this.text:SetText( text )
 			this.timeToNextUpdate = toNextUpdate
 		else
@@ -270,28 +273,28 @@ end
 --[[ Shine Codes ]]--
 function CooldownCount:StartToShine(textFrame)
 	local shineFrame = textFrame.shine or CooldownCount:CreateShineFrame(textFrame:GetParent())
-
+	
 	shineFrame.shine:SetAlpha(shineFrame:GetParent():GetAlpha())
 	shineFrame.shine:SetHeight(shineFrame:GetHeight() * CooldownCount.db.profile.shineScale)
 	shineFrame.shine:SetWidth(shineFrame:GetWidth() * CooldownCount.db.profile.shineScale)
-
+	
 	shineFrame:Show()
 end
 
 function CooldownCount:CreateShineFrame(parent)
 	local shineFrame = CreateFrame("Frame", nil, parent)
 	shineFrame:SetAllPoints(parent)
-
+	
 	local shine = shineFrame:CreateTexture(nil, "OVERLAY")
 	shine:SetTexture("Interface\\Cooldown\\star4")
 	shine:SetPoint("CENTER", shineFrame, "CENTER")
 	shine:SetBlendMode("ADD")
 	shineFrame.shine = shine
-
+	
 	shineFrame:Hide()
 	shineFrame:SetScript("OnUpdate", CooldownCount.Shine_Update)
 	shineFrame:SetAlpha(parent:GetAlpha())
-
+	
 	return shineFrame
 end
 
@@ -299,7 +302,7 @@ function CooldownCount:Shine_Update()
 	local shine = this.shine
 	local alpha = shine:GetAlpha()
 	shine:SetAlpha(alpha * 0.95)
-
+	
 	if alpha < 0.1 then
 		this:Hide()
 	else
---
+++ CooldownCount/Locale/Locale.ru.lua
@@ -0,0 +1,48 @@
+﻿-- Russian localization by Lichery
+
+local L = AceLibrary("AceLocale-2.2"):new("CooldownCount")
+
+L:RegisterTranslations("ruRU", function() return {
+	["reset"] = "Сброс",
+	["Reset all settings."] = "Сброс всех настроек.",
+	["Reset to default settings."] = "Сброс к настройка по умолчанию.",
+	["shine"] = "Вспышка",
+	["Toggle icon shine display after finish cooldown."] = "Переключение отображения вспышки иконок после завершения перезарядки.",
+	["shinescale"] = "Масштаб вспышки",
+	["Adjust icon shine scale."] = "Настройка масштаба вспышки иконок.",
+	["font"] = "Шрифт",
+	["Set cooldown value display font."] = "Установить шрифт перезарядки.",
+	
+	["font size"] = "Размер шрифта",
+	["Setup cooldown value display font size."] = "Установить размер шрифта перезарядки.",
+	["small size"] = "Маленький размер",
+	["Small font size for cooldown is longer than 10 minutes."] = "Маленький размер шрифта для перезарядки больше, чем 10 минут.",
+	["medium size"] = "Средний размер",
+	["Medium font size for cooldown is longer than 1 minute and less than 10 minutes."] = "Средний размер шрифта для перезарядки больше 1 минуты и меньше 10 минут.",
+	["large size"] = "Большой размер",
+	["Large font size for cooldown is longer than 10 seconds and less than 1 minutes."] = "Большой размер шрифта для перезарядки больше 10 секунд и меньше 1 минуты.",
+	["warning size"] = "Размер предупреждения",
+	["Warning font size for cooldown is less than 10 seconds."] = "Размер шрифта предупреждения для перезарядки меньше 10 секунд.",
+	
+	["common color"] = "Общий цвет",
+	["Setup the common color for value display."] = "Настройка общего цвета для отображения значений.",
+	["warning color"] = "Цвет предупреждения",
+	["Setup the warning color for value display."] = "Настройка цвета предупреждения для отображения значений.",
+	
+	["d"] = "д",
+	["h"] = "ч",
+	["m"] = "м",
+	
+--	["Fonts\\FRIZQT__.TTF"] = "Fonts\\FRIZQT__.TTF",
+	
+	["minimum duration"] = "Минимальная длительность",
+	["Minimum duration for display cooldown count."] = "Минимальная длительность для отображения количества перезарядки.",
+	["hide animation"] = "Скрыть анимацию",
+	["Hide Bliz origin cooldown animation."] = "Скрыть оригинальную анимацию перезарядки Blizzard.",
+-- Для изменения шрифта:	
+--	["2002"] = "Fonts\\2002.TTF", 
+--	["FRIZQT__"] = "Fonts\\FRIZQT__.TTF", 
+--	["ARIALN"] = "Fonts\\ARIALN.TTF", 
+--	["K_Damage"] = "Fonts\\K_Damage.TTF", 
+--	["K_Pagetext"] = "Fonts\\K_Pagetext.TTF", 
+} end)
